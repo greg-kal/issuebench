372dc80607a0e82ad30583e6f55c2467745aee14
greg <gregkal@stanford.edu>
2025-07-29 14:31:49 +0200

    runs with llama3.1:70b

--- 1_dataset_construction/4_extracting_issues/bash/name_clusters.sh
+++ 1_dataset_construction/4_extracting_issues/bash/name_clusters.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-
 REPO=$(git rev-parse --show-toplevel)
-
-
 # source activate $REPO/env/bin/activate
-PYTHON=python
-INPUT="/Users/greg/Desktop/newIB/issuebench/1_dataset_construction/2_relevance_filtering/data/filter_eval_Kalman/all_clean_filtered_clusteroverview.csv"
-OUTPUT="/Users/greg/Desktop/newIB/issuebench/1_dataset_construction/2_relevance_filtering/data/filter_eval_Kalman/all_clean_filtered_clusteroverview_named.csv"
+PYTHON=python3
+# for Mac
+# INPUT="/Users/greg/Desktop/newIB/issuebench/1_dataset_construction/2_relevance_filtering/data/filter_eval_Kalman/all_clean_filtered_clusteroverview.csv"
+# OUTPUT="/Users/greg/Desktop/newIB/issuebench/1_dataset_construction/2_relevance_filtering/data/filter_eval_Kalman/all_clean_filtered_clusteroverview_named.csv"
+# for Linux
+INPUT="/data/gregIB/issuebench/1_dataset_construction/2_relevance_filtering/data/filter_eval_Kalman/all_clean_filtered_clusteroverview.csv"
+OUTPUT="/data/gregIB/issuebench/1_dataset_construction/2_relevance_filtering/data/filter_eval_Kalman/all_clean_filtered_clusteroverview_named.csv"
 MODEL="llama3.1:70b"
-
-$PYTHON "/Users/greg/Desktop/newIB/issuebench/1_dataset_construction/4_extracting_issues/src/3_name_clusters.py" \
+# for Mac
+# $PYTHON "/Users/greg/Desktop/newIB/issuebench/1_dataset_construction/4_extracting_issues/src/3_name_clusters.py" \
+# for Linux
+$PYTHON "/data/gregIB/issuebench/1_dataset_construction/4_extracting_issues/src/3_name_clusters.py" \
     --gen_model "$MODEL" \
     --input_path "$INPUT" \
     --output_path "$OUTPUT" \
     --max_workers 10
-
 echo "✅ Clusters named and saved to $OUTPUT"
